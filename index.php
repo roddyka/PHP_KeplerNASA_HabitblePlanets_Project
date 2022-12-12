6b15095a95fde19b2da58af349dcba733938c3ff
rodrigo antunes <rodrigoka@live.com>
2022-12-12 20:14:13 +0000

    update

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 
-include_once('HabitablePlanets.php');
+include_once('HabitablePlanetsClass.php');
 $habitable_planets = new HabitablePlanetsClass('kepler_data.csv');
 var_dump($habitable_planets->get_habitable_planets());
